b33b512950bc9f1bcfdde9ee98387a9ef13bd053
Fahddeya <107079165+Fahddeya@users.noreply.github.com>
2024-11-03 19:54:52 +0200

    FINAAAL ISA

--- volemachine7.cpp
+++ volemachine7.cpp
@@ -572,17 +572,35 @@ int main() {
         cout << "3) Display data.\n";
         cout << "4) Exit.\n";
         cin >> choice;
-        while(choice < 1 || choice > 4){
-            cout << "Invalid choice, please try again.\n";
+        while (true) {
             cin >> choice;
+
+            if (cin.fail()) {
+                cout << "Invalid input!\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            } else if (choice < 1 || choice > 4) {
+                cout << "Invalid choice, please try again.\n";
+            } else {
+                break;
+            }
         }
         switch(choice){
             case 1:
                 cout << "Enter the starting location for storing data in memory (0 < location < 256):\n";
-                cin >> pc;
-                while(pc < 1 || pc > 256){
-                    cout << "Invalid choice, please try again.\n";
+                while (true) {
                     cin >> pc;
+                    if (cin.fail()){
+                        cout << "Invalid input! Please enter a number between 1 and 256.\n";
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    }
+                    else if(pc < 1 || pc > 256){
+                        cout << "Invalid choice, please try again.\n";
+                    }
+                    else{
+                        break;
+                    }
                 }
                 mc = Machine(pc);
                 while(!mc.Readfile(pc)){
